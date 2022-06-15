df75c7eef6eb953b6ccf545c45feca58ea002e8f
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-06-15 08:45:40 +0300

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded',function(){  console.log("The DOM has loaded");
+
+const form = document.getElementById('create-task-form')
+form.addEventListener('submit', (e)=>{e.preventDefault()
+handleMyTask(e.target['new-task-description'].value)
+form.reset()
+})
+})
+
+function handleMyTask(myParameter){
+  const li = document.createElement('li')
+  li.style.listStyle = 'none'
+  li.textContent = `${myParameter}`
+  const btn = document.createElement('button')
+  btn.addEventListener('click',deleteButton)
+  btn.textContent = "X"
+ 
+  const ul = document.getElementById('tasks')
+  ul.appendChild(li)
+
+
+}
+
+function deleteButton(e){
+e.target.parentNode.remove()
+}
